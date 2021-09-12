259c5281571094078757cdb88474c23f340a917d
Rutwik87 <47714147+Rutwik87@users.noreply.github.com>
2021-09-12 19:14:52 +0530

    Done all features

---
+++ app.js
@@ -0,0 +1,38 @@
+var buyPrice = document.querySelector("#initial-price")
+var currentPrice = document.querySelector("#current-price")
+var quantity = document.querySelector("#quantity")
+
+const calculateBtn = document.querySelector("#calculate-btn")
+const output = document.querySelector("#output-msg")
+
+function clickHandler(){
+    var costPrice = buyPrice.value;
+    var sellPrice = currentPrice.value; 
+    var profitPerStock = sellPrice - costPrice;
+    var absProfit = computeAbsoluteProfit(profitPerStock,quantity.value);
+    var profitPercent = computeProfitPercent(costPrice,sellPrice);
+    displayOutput(absProfit,profitPercent);
+}
+
+function displayOutput(absProfit,profitPercent){
+    if(absProfit == 0){
+        output.innerText = "No Profit No Loss"
+    } 
+    else if(absProfit < 0){
+        output.innerText = `Currently Loss is ${-1*absProfit} and loss percent is ${-1*profitPercent}%`
+    }
+    else{
+        output.innerText = `Currently Profit is ${absProfit} and profit percent is ${profitPercent}%`
+    }
+    
+}
+
+function computeAbsoluteProfit(profitPerStock,quantity){
+    return profitPerStock*quantity;
+}
+
+function computeProfitPercent(cost,sell){
+    return (sell-cost)*100/cost;
+}
+
+calculateBtn.addEventListener("click",clickHandler);
